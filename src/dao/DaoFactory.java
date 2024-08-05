041b13928b417ac1c4c4024aa76af49d4f3c8984
Adams <adams.contato@gmail.com>
2024-08-05 20:36:32 -0300

    findById implementation

---
+++ src/dao/DaoFactory.java
@@ -0,0 +1,13 @@
+package dao;
+
+import db.DB;
+import entities.Seller;
+import impl.SellerDaoJDBC;
+
+public class DaoFactory {
+
+    public static SellerDao createSellerDao(){
+        return new SellerDaoJDBC(DB.getConnection());
+    }
+
+}
